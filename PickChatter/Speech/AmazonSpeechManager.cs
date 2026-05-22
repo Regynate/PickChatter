3255a3d01b3e8372229f99e1d197e858b87e90d3
Regynate <64607261+Regynate@users.noreply.github.com>
2026-05-22 15:05:05 +0300

    Update voices

--- PickChatter/Speech/AmazonSpeechManager.cs
+++ PickChatter/Speech/AmazonSpeechManager.cs
@@ -8,27 +8,15 @@ namespace PickChatter
 {
     internal sealed class AmazonSpeechManager : StreamElementsSpeechManager
     {
-        public override List<string> AvailableVoices => new() {
-            "Russell",
-            "Nicole",
-            "Linda",
-            "Heather",
-            "Emma",
-            "Brian",
-            "Amy",
-            "Geraint",
-            "Sean",
-            "Raveena",
-            "Aditi",
-            "Salli",
-            "Matthew",
-            "Kimberly",
-            "Kendra",
-            "Justin",
-            "Joey",
-            "Joanna",
-            "Ivy"
-        };
+        public override List<string> AvailableVoices => [
+            "Isabelle", "Danielle", "Gregory", "Burcu", "Jitka", "Sabrina", "Joanna", "Ruth", "Lupe", "Jasmine", "Jihye", "Kevin", "Filiz", "Elin", "Astrid",
+            "Tatyana", "Maxim", "Carmen", "Ines", "Cristiano", "Vitoria", "Ricardo", "Camila", "Maja", "Jan", "Jacek", "Ewa", "Ola", "Lisa", "Ruben", "Lotte",
+            "Laura", "Ida", "Liv", "Seoyeon", "Kazuha", "Tomoko", "Takumi", "Mizuki", "Bianca", "Giorgio", "Carla", "Karl", "Dora", "Mathieu", "Lea", "Celine",
+            "Chantal", "Gabrielle", "Penelope", "Miguel", "Mia", "Lucia", "Enrique", "Conchita", "Geraint", "Salli", "Matthew", "Kimberly", "Kendra", "Justin",
+            "Joey", "Ivy", "Aria", "Ayanda", "Raveena", "Aditi", "Emma", "Brian", "Amy", "Russell", "Nicole", "Olivia", "Vicki", "Marlene", "Hans", "Naja",
+            "Mads", "Sofie", "Gwyneth", "Zhiyu", "Zeina", "Hala", "Arlet", "Hannah", "Stephen", "Kajal", "Hiujin", "Suvi", "Niamh", "Tiffany", "Arthur", "Daniel",
+            "Liam", "Pedro", "Sergio", "Andres", "Remi", "Adriano", "Thiago", "Zayd", "Lennart", "Ambre", "Florian", "Beatrice", "Lorenzo"
+        ];
 
         protected override string VoiceID => CurrentVoice;
 
